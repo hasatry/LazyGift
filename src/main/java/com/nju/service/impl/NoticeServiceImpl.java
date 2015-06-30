fff3a74d43b3dfd27d5a9a5ad16680fb6f1803e3
liveangel <jiangshu.social@gmail.com>
2015-06-30 13:27:15 +0800

    显示公告：输出查询日志

--- src/main/java/com/nju/service/impl/NoticeServiceImpl.java
+++ src/main/java/com/nju/service/impl/NoticeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.nju.service.impl;
 
 import com.nju.data.dao.OrderDao;
+import com.nju.data.dao.impl.OrderDaoImpl;
 import com.nju.model.Order;
 import com.nju.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,8 @@ import java.util.List;
  * Created by liveangel on 15-6-29.
  */
 public class NoticeServiceImpl implements NoticeService {
-    @Autowired
-    private OrderDao orderDao;
+    //@Autowired
+    private OrderDao orderDao = new OrderDaoImpl();
 
     @Override
     public String getHistoryLocation(int begin, int end, String dateBegin, String dateEnd) {
